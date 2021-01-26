49d220e8e9848ff561438ab70b78bb8f940f6d73
GODCREATOR <974980621@qq.com>
2021-01-26 23:15:37 +0800

    check ignore

--- Output/cc936.d
+++
@@ -1,4 +0,0 @@
-..\..\output\cc936.o: ..\..\User\FATFS\option\cc936.c
-..\..\output\cc936.o: ..\..\User\FATFS\option\../ff.h
-..\..\output\cc936.o: ..\..\User\FATFS\option\../integer.h
-..\..\output\cc936.o: ..\..\User\FATFS\option\../ffconf.h
